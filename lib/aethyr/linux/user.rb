373c309853710f2f7994f7202544a4752ef8ca7d
Troy Stribling <troy.stribling@gmail.com>
2008-07-20 18:59:34 +0000

    adding user linux interface

--- lib/aethyr/linux/user.rb
+++ lib/aethyr/linux/user.rb
@@ -22,8 +22,11 @@ module Aethyr
         ##########################################################################################################
         def find_all
           users = `cat /etc/passwd`
-          users.split("\n").each do |u|
-            
+          users.split("\n").collect do |u|
+            ua = u.split(':')
+            gecos = ua[4].split(',')
+            {:name => ua[0], :uid => ua[2], :default_gid => ua[3], :home_directory => ua[5], :login_shell => ua[6], 
+             :full_name => gecos[0], :office => gecos[1], :extension => gecos[2], :home_phone => gecos[3]}
           end
         end
   
