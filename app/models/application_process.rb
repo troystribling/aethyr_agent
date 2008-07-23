bdf0db92ba18df99def6f330a5940e42c80ca0c4
Troy Stribling <troy.stribling@gmail.com>
2008-07-23 12:49:40 +0000

    debugging process and thread object models

--- app/models/application_process.rb
+++ app/models/application_process.rb
@@ -7,6 +7,11 @@ class ApplicationProcess < ActiveRecord::Base
   has_ancestor :named => :aln_resource   
 
   ######################################################################################################
+  #### mixins
+  include Aethyr::Mixins::Synchronizer::Model
+  extend Aethyr::Aln::ConnectedModelHelper  
+
+  ######################################################################################################
   #### virtual attributes
   attr_accessor :user, :ppid
 
@@ -17,12 +22,54 @@ class ApplicationProcess < ActiveRecord::Base
 
   ####################################################################################################
   def add_associations(supporter)
+
+    #### supporter relation
+    supporter << self
     
+  end
+
+  ######################################################################################################
+  def synchronize_models(supporter, local_models, remote_models) 
+    model_sync_key = self.sync_key(params)
+    model = local_models[model_sync_key]
+    remote_models.each{|params| synchronize_model(model, params)}
+    remote_models.each{|params| synchronize_model_associations(supporter, model)}
+  end
+
+  ######################################################################################################
+  def synchronize_model(model, params) 
+    if model.nil?
+      model = self.new(params)
+      model.save
+      model.synched = false
+    else 
+      model.reload
+      model.attributes = params
+      model.synched = true
+      model.save
+    end
+  end
+
+  ######################################################################################################
+  def synchronize_model_associations(supporter, model) 
+    unless model.synched
+      model.reload
+      model.add_associations(supporter)
+      supporter.reload unless supporter.nil?
+    end
   end
 
   ######################################################################################################
   #### class methods
   class << self
+    
+    ####################################################################################################
+    def sync_key(params)
+p params      
+      params[:name] = params[:pid]
+      params[:name]
+    end
+    
   end  
 
 ######################################################################################################
