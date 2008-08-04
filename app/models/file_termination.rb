4880f1f49a4bb05f24a037e97430e42689e87991
Troy Stribling <troy.stribling@gmail.com>
2008-08-04 13:05:45 +0000

    starting files

--- app/models/file_termination.rb
+++ app/models/file_termination.rb
@@ -3,12 +3,17 @@
 class FileTermination < ActiveRecord::Base
 
   ######################################################################################################
+  #### mixins
+  include Aethyr::Mixins::Synchronizer::Model
+  extend Aethyr::Aln::ConnectedModelHelper  
+
+  ######################################################################################################
   #### inheritance relations
   has_ancestor :named => :aln_termination   
 
   ######################################################################################################
   #### virtual attributes
-  attr_accessor :pid, :user, :device
+  attr_accessor :pid, :device
 
   ######################################################################################################
   #### validation
