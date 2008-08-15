32e119abeb35b055751a36daf541006a476d32be
Troy Stribling <troy.stribling@gmail.com>
2008-08-15 01:37:00 +0000

    adding network interface views and controllers

--- lib/aethyr/mixins/helpers.rb
+++ lib/aethyr/mixins/helpers.rb
@@ -39,10 +39,23 @@ module Aethyr
       end
   
       ######################################################################################################
-      def display_list_attribute(attribute, value, options = {})
+      def display_list(model, &block)      
+        concat('<div class="display-list-wrapper">', block.binding)
+        concat('<table class="display-list">', block.binding)
+        yield(model)
+        concat('</table>', block.binding)
+        concat("</div>", block.binding)
+      end
+  
+      ######################################################################################################
+      def display_list_attribute(a1, a2, options = {})
         attribute_options = {:class => "display-list-attribute"}
         value_options = {:class => "display-list-value"}
-        item = content_tag(:td, attribute, attribute_options) + content_tag(:td, value, value_options)       
+        unless a1.kind_of?(String)
+          item = content_tag(:td, a2.to_s, attribute_options) + content_tag(:td, a1.send(a2), value_options)  
+        else
+          item = content_tag(:td, a1, attribute_options) + content_tag(:td, a2, value_options)  
+        end      
         content_tag :tr, item, options 
       end
   
